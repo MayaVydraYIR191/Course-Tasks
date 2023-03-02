259256a52c86660f696f5370d32712628a02696c
MayaVydraYIR191 <vydra_mo_19@mf.grsu.by>
2023-03-02 12:39:31 +0300

    The newest Home Task!

---
+++ BooksTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrintable[] booksmagazines = new IPrintable[5];
+            for (int i = 0; i < booksmagazines.Length; i++)
+            {
+                Console.WriteLine(booksmagazines[i]);
+            }
+        }
+    }
+    interface IPrintable
+    {
+        void Print()
+        {
+            Console.WriteLine();
+        }
+    }
+    class Book : IPrintable
+    {
+        public string bookname;
+        public int booknumber;
+        static void PrintBooks(IPrintable [] printable)
+        {
+            Console.WriteLine();
+        }
+    }
+    class Magazine : IPrintable
+    {
+        public string magname;
+        public int magnumber;
+        static void PrintMagazines(IPrintable[]printable)
+        {
+            Console.WriteLine();
+        }
+    }
+
+
+}
---
+++ MusicTask.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the word");
+            string play = Console.ReadLine();
+            switch (play)
+            {
+                case "Guitar":
+                    Guitar.Play();
+                    break;
+
+                case "Violin":
+                    Violin.Play();
+                    break;
+                case "Piano":
+                    Piano.Play();
+                    break;
+                default:
+                    Console.WriteLine("Wrong");
+                    break;
+            }
+
+        }
+    }
+
+    interface IPlayable
+    {
+        public static void Play()
+        {
+            Console.WriteLine("I play");
+        }
+    }
+
+    public class Guitar : IPlayable
+    {
+        public static void Play()
+        {
+            Console.WriteLine("I am guitar and I am playing");
+        }
+    }
+    public class Violin : IPlayable
+    {
+        public static void Play()
+        {
+            Console.WriteLine("I am violin and I am playing");
+        }
+    }
+    public class Piano : IPlayable
+    {
+        public static void Play()
+        {
+            Console.WriteLine("I am piano and I am playing");
+        }
+    }
+
+
+
+
+}
