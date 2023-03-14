2b98374f4cc9e197522a733ee47af69fde9b92e6
MayaVydraYIR191 <vydra_mo_19@mf.grsu.by>
2023-03-14 13:34:12 +0300

    The latest new homework!

---
+++ LibraryTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //окей тут что-нибудь да будет  
+        }
+    }
+    public class Library
+    {
+        public string BookName;
+        public int BookNum;
+        public string BookDescription;
+        public bool BookGet;
+        public void BookIsHere(string bookname)
+        {
+            if(BookGet == true)
+            {
+                bookname = BookName;
+                Reader.BookGetting(bookname);
+            }
+        }
+    }
+    public class Reader
+    {
+        public static bool BookHere;
+        public string readername;
+        public delegate void Waiting(string message);
+        public static event Waiting WaitOver;
+        public static void BookGetting(string reader)
+        {
+            if (BookHere == true)
+            {
+                WaitOver?.Invoke("You book is finally here");
+            }
+        }
+    }
+}
---
+++ UnitTask.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    abstract class Units
+    {
+        string unitName;
+        int unitNumber;
+        string[] unitAbilities;
+
+        public bool MoveRight;
+        public bool MoveLeft;
+        public bool Jump;
+        public bool Stopping;
+        void Move()
+        {
+            string command = Console.ReadLine();
+            switch (command)
+            {
+                case "Right":
+                    MoveRight = true;
+                    Console.WriteLine("Character is moving right");
+                    break;
+                case "Left":
+                    MoveLeft = true;
+                    Console.WriteLine("Character is moving left");
+                    break;
+                case "Jump":
+                    Jump = true;
+                    Console.WriteLine("Character is jumping");
+                    break;
+                case "Stop":
+                    Stopping = true;
+                    Console.WriteLine("Character stops in place");
+                    break;
+            }
+        }
+        public abstract void Stand();
+    }
+    interface IMagicalAbilities
+    {
+        static string Fireball(string ability)
+        {
+            ability = Console.ReadLine();
+            if(ability == "Fireball")
+            {
+                Console.WriteLine("The raging fire essence is thrown into an enemy");
+            }
+            else if (ability == "Fly")
+            {
+                ability = IFlight.Flying(ability);
+            }
+            else if (ability == "Swim")
+            {
+                ability = ISwim.Swimming(ability);
+            }
+            return (ability);
+        }
+    }
+    interface IFlight
+    {
+        static string Flying(string ability)
+        {
+            ability = Console.ReadLine();
+            if (ability == "Fly")
+            {
+                Console.WriteLine("Character bursts into the air, opening its wings");
+            }
+            else if (ability == "Swim")
+            {
+                ability = ISwim.Swimming(ability);
+            }
+            else if (ability == "Fireball")
+            {
+                ability = IMagicalAbilities.Fireball(ability);
+            }
+            return (ability);
+        }
+    }
+    interface ISwim
+    {
+        static string Swimming(string ability)
+        {
+            ability = Console.ReadLine();
+            if (ability == "Swim")
+            {
+                Console.WriteLine("Character jumps into the water and swims");
+            }
+            else if(ability == "Fly")
+            {
+                ability = IFlight.Flying(ability);
+            }
+            else if(ability == "Fireball")
+            {
+                ability = IMagicalAbilities.Fireball(ability);
+            }
+            return (ability);
+        }
+    }
+
+
+}
