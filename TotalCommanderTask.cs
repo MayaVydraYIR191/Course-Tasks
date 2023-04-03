1a90ce96083a645242a2fa27f4587356e31c9319
MayaVydraYIR191 <vydra_mo_19@mf.grsu.by>
2023-04-03 10:12:34 +0300

    The last 8th homework

---
+++ TotalCommanderTask.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "file";
+            string directory = "directory";
+            string copyfile = "copyfile";
+            string renamefile = "renamefile";
+            string replace = "replace";
+
+            TotalCommander.FileCreating(file);
+            TotalCommander.FileCreating(copyfile);
+            TotalCommander.FileCreating(renamefile);
+            TotalCommander.FileCreating(replace);
+            TotalCommander.DireCreating(directory);
+            TotalCommander.Copying(file, copyfile);
+            TotalCommander.Renaming(file, renamefile, replace);
+            TotalCommander.DireDestroying(directory);
+            TotalCommander.FileDestroying(file);
+
+        }
+    }
+
+    public class TotalCommander
+    {
+        public static void FileCreating(string file)
+        {
+            if(!File.Exists(file))
+            {
+                File.Create(file);
+            }
+        }
+
+        public static void FileDestroying(string file)
+        {
+            if(File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+        public static void DireCreating(string directory)
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        public static void DireDestroying(string directory)
+        {
+            Directory.Delete(directory);
+        }
+
+        public static void Copying(string file, string copyfile)
+        {
+            if(File.Exists(file))
+            {
+                File.Copy(file,copyfile,true);
+            }
+        }
+        public static void Renaming(string file, string renamefile,string replace)
+        {
+            if(File.Exists(file))
+            {
+                File.Replace(file,renamefile,replace);
+            }
+        }
+    }
+
+}
---
+++ dz8dllTask.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System;
+using System.IO;
+
+namespace ConsoleApp69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dz8 = Assembly.Load(File.ReadAllBytes("dz8.dll"));
+            Type dz8type = dz8.GetType();
+            Console.WriteLine($"Name: {dz8type.Name}");
+            Console.WriteLine($"Full Name: {dz8type.FullName}");
+            Console.WriteLine($"Namespace: {dz8type.Namespace}");
+            Console.WriteLine($"Is struct: {dz8type.IsValueType}");
+            Console.WriteLine($"Is class: {dz8type.IsClass}");
+
+            var instance = Activator.CreateInstance(dz8type);
+            Console.WriteLine($"Email: {dz8type.GetProperty("Email").GetValue(instance, null)}");
+        }
+    }
+}
